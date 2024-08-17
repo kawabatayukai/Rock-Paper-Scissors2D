99cec3ba6cecdc6d4cb80eb87b4beb8434ff34d9
kawabatayukai <107020988+kawabatayukai@users.noreply.github.com>
2024-08-17 09:38:42 +0900

    Enemy配置してみた
    Add:class PatrolPoint
    
    ・characterの派生クラスclass Enemyを実装
    ・overrideメソッドの実装のみ
    ・GroundChecker, Characterの派生先での動作を確認
    ・行動パターン制御の準備として、巡回ポイント"PatrolPoint"実装
    ・巡回地点をOnDrawGizmosで仮描画

---
+++ Assets/Project/Scripts/Characters/Enemy/Enemy.cs
@@ -0,0 +1,17 @@
+﻿public class Enemy : Character
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
---
+++ Assets/Project/Scripts/Characters/Enemy/PatrolSystem/PatrolPoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+// 巡回ポイント
+
+namespace PatrolSystem
+{
+    public class PatrolPoint : MonoBehaviour
+    {
+        [SerializeField]
+        int number;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(transform.position, 0.1f);
+            Gizmos.DrawLine(transform.position, transform.position + (Vector3.up * 1f));
+            Gizmos.DrawSphere(transform.position + (Vector3.up * 1f), 0.15f);
+        }
+    }
+}
+
+
